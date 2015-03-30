b47d1aacb88d543fbb7e3de20683ae6a9ebba96c
Kenfack Valmy-Roi <roykenvalmy@gmail.com>
2015-03-30 17:07:54 +0100

    Ajout de findProgramme pour un parcours à une année académique donnée et un semestre

--- src/main/java/com/douwe/notes/dao/impl/ProgrammeDaoImpl.java
+++ src/main/java/com/douwe/notes/dao/impl/ProgrammeDaoImpl.java
@@ -3,9 +3,11 @@ package com.douwe.notes.dao.impl;
 import com.douwe.generic.dao.DataAccessException;
 import com.douwe.generic.dao.impl.GenericDao;
 import com.douwe.notes.dao.IProgrammeDao;
+import com.douwe.notes.entities.AnneeAcademique;
 import com.douwe.notes.entities.Niveau;
 import com.douwe.notes.entities.Option;
 import com.douwe.notes.entities.Programme;
+import com.douwe.notes.entities.Semestre;
 import java.util.List;
 
 /**
@@ -23,10 +25,10 @@ public class ProgrammeDaoImpl extends GenericDao<Programme, Long> implements IPr
         return getManager().createNamedQuery("Programme.findAllActive").getResultList();
     }
 
+
     @Override
-    public Programme findByNiveauOption(Niveau n, Option o) throws DataAccessException {
-      return (Programme)(getManager().createNamedQuery("Programme.findByNiveauOption").setParameter("param1", n.getId()).setParameter("param2", o.getId()).getSingleResult());
-       
+    public List<Programme> findByNiveauOption(Niveau n, Option o, AnneeAcademique academique, Semestre semestre) throws DataAccessException {
+        return getManager().createNamedQuery("Programme.findByNiveauOption").setParameter("param1", n).setParameter("param2", o).setParameter("param3", academique).setParameter("param4", semestre).getResultList();
     }
     
 }
--- src/main/java/com/douwe/notes/service/impl/ProgrammeServiceImpl.java
+++ src/main/java/com/douwe/notes/service/impl/ProgrammeServiceImpl.java
@@ -1,10 +1,16 @@
 package com.douwe.notes.service.impl;
 
 import com.douwe.generic.dao.DataAccessException;
+import com.douwe.notes.dao.IAnneeAcademiqueDao;
+import com.douwe.notes.dao.INiveauDao;
+import com.douwe.notes.dao.IOptionDao;
 import com.douwe.notes.dao.IProgrammeDao;
+import com.douwe.notes.dao.ISemestreDao;
+import com.douwe.notes.entities.AnneeAcademique;
 import com.douwe.notes.entities.Niveau;
 import com.douwe.notes.entities.Option;
 import com.douwe.notes.entities.Programme;
+import com.douwe.notes.entities.Semestre;
 import com.douwe.notes.service.IProgrammeService;
 import com.douwe.notes.service.ServiceException;
 import java.util.List;
@@ -22,6 +28,18 @@ public class ProgrammeServiceImpl implements IProgrammeService {
 
     @Inject
     private IProgrammeDao programmeDao;
+    
+    @Inject
+    private INiveauDao niveauDao;
+    
+    @Inject
+    private IOptionDao optionDao;
+    
+    @Inject
+    private IAnneeAcademiqueDao academiqueDao;
+    
+    @Inject
+    private ISemestreDao semestreDao;
 
     public IProgrammeDao getProgrammeDao() {
         return programmeDao;
@@ -80,24 +98,40 @@ public class ProgrammeServiceImpl implements IProgrammeService {
         }
     }
 
-    @Override
-
-    public Programme findByNiveauOption(Niveau n, Option o) throws ServiceException {
-        try {
-            Programme programme = programmeDao.findByNiveauOption(n, o);
-            if(programme != null){
-                return programme;
-            }
-            return null;
-        } catch (DataAccessException ex) {
-            Logger.getLogger(ProgrammeServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-            throw  new ServiceException("La ressource demandée est introuvable");
-        }
-    }
 
     public List<Programme> findProgrammeByParcours(Long niveauId, Long optionId) throws ServiceException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 
     }
+
+    @Override
+    public List<Programme> findProgrammeByParcours(Long niveauId, Long optionId, Long idAnnee, Long idSemestre) throws ServiceException {
+        try {
+            Niveau niveau = niveauDao.findById(niveauId);
+            if(niveau == null){
+                throw  new ServiceException("Ressource introuvable");
+            }
+            
+            Option option = optionDao.findById(optionId);
+            if(option == null){
+                throw new ServiceException("Le service demandé est introuvable");
+            }
+            
+            AnneeAcademique academique = academiqueDao.findById(idAnnee);
+            if(academique == null){
+                throw new ServiceException("");
+            }
+            
+            Semestre semestre = semestreDao.findById(idSemestre);
+            if(semestre == null){
+                throw  new ServiceException("");
+            }
+            return programmeDao.findByNiveauOption(niveau, option, academique, semestre);
+        } catch (DataAccessException ex) {
+            Logger.getLogger(ProgrammeServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            throw new ServiceException("");
+        }
+    }
+
 
 }
