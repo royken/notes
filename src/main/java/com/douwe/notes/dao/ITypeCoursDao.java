a0e4c87b4011a5cc7bbe25f290bac1c0d6a3308a
Kenfack Valmy-Roi <roykenvalmy@gmail.com>
2015-04-10 11:01:49 +0100

    Modification de findAll de TypeCous

--- src/main/java/com/douwe/notes/dao/ITypeCoursDao.java
+++ src/main/java/com/douwe/notes/dao/ITypeCoursDao.java
@@ -1,12 +1,16 @@
 package com.douwe.notes.dao;
 
+import com.douwe.generic.dao.DataAccessException;
 import com.douwe.generic.dao.IDao;
 import com.douwe.notes.entities.TypeCours;
+import java.util.List;
 
 /**
  *
  * @author Vincent Douwe <douwevincent@yahoo.fr>
  */
 public interface ITypeCoursDao extends IDao<TypeCours, Long>{
+    
+    public List<TypeCours> findAllActive() throws DataAccessException;
     
 }
